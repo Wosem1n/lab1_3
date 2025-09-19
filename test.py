851c66f5d5af4f023ef06131f65c3b3f4e338027
cumavstvo <147176789+Nyformal@users.noreply.github.com>
2025-09-19 10:19:20 +0500

    add test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import lib
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+list3 = [5, 6, 7, 8, 9]
+
+print(f"Количество элементов во всех списках: {lib.count_common_elements(list1, list2, list3)}")
